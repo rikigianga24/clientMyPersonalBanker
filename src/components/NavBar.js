9dbb5a128dc55b008f11821932a1123f7f5632e0
rikigianga24 <riccardo.giangani@hotmail.com>
2021-05-04 15:01:56 +0200

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Nav, Navbar, FormControl , Link , Form , Button} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavBar(){
+    return(
+        <>
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">myPersonalBanker</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Gestisci il tuo saldo</Nav.Link>
+      <Nav.Link href="#pricing">Transazione</Nav.Link>
+    </Nav>
+  </Navbar>
+      </>
+    )
+    
+}
+export default NavBar;
